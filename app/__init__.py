d0691eee31197b0cfb6e987043a96fd3c3445c67
ZakAbdu <zakariaabdullahi1@gmail.com>
2024-01-08 15:31:13 -0800

    sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask_smorest import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from Config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 api = Api(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models.BookModel import BookModel
 
 from resources.movies import bp as movies_bp
 api.register_blueprint(movies_bp)
---
+++ models/MovieModel.py
@@ -0,0 +1,11 @@
+# from app import db
+
+# class MovieModel(db.Model):
+
+#     __tablename__ = 'movies'
+
+#     id = db.Column(db.Integer, primary_key = True)
+#     author = db.Column(db.String(100), nullable = False)
+#     publisher = db.Column(db.String(100), nullable = False)
+#     title = db.Column(db.String(100), nullable = False)
+#     year = db.Column(db.String(4), nullable = False)
